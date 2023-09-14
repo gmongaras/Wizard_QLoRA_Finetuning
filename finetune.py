edee02478b7117034eccc19f822eba21b861ac1f
Gabriel Mongaras <gabriel@mongaras.com>
2023-09-14 16:13:23 -0500

    Init

---
+++ finetune.py
@@ -0,0 +1,234 @@
+from datasets import load_dataset, load_from_disk
+from peft import LoraConfig, get_peft_model
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    AutoTokenizer,
+    TrainingArguments,
+    Trainer,
+)
+
+
+
+max_length = 128
+
+
+# Model loading params
+load_in_4bit = True
+
+# LoRA Params
+lora_alpha = 16             # How much to weigh LoRA params over pretrained params
+lora_dropout = 0.1          # Dropout for LoRA weights to avoid overfitting
+lora_r = 16                 # Bottleneck size between A and B matrix for LoRA params
+lora_bias = "all"           # "all" or "none" for LoRA bias
+model_type = "llama"        # falcon or llama
+dataset_type = "reddit"      # "squad" or "reddit" or "reddit_negative"
+lora_target_modules = [     # Which modules to apply LoRA to (names of the modules in state_dict)
+    "query_key_value",
+    "dense",
+    "dense_h_to_4h",
+    "dense_4h_to_h",
+] if model_type == "falcon" else [
+    "q_proj",
+    "k_proj",
+    "v_proj",
+    "o_proj",
+    "gate_proj",
+    "up_proj",
+    "down_proj"
+]
+
+# Trainer params
+output_dir = "outputs_reddit_neg"                              # Directory to save the model
+optim_type = "adafactor"                            # Optimizer type to train with 
+learning_rate = 0.00005                              # Model learning rate
+weight_decay = 0.002                                # Model weight decay
+per_device_train_batch_size = 8                     # Train batch size on each GPU
+per_device_eval_batch_size = 8                      # Eval batch size on each GPU
+gradient_accumulation_steps = 2                     # Number of steps before updating model
+warmup_steps = 5                                    # Number of warmup steps for learning rate
+save_steps = 100                                     # Number of steps before saving model
+logging_steps = 25                                  # Number of steps before logging
+
+
+
+
+
+
+
+
+# Load in the model as a 4-bit or 8-bit model
+if load_in_4bit == True:
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype="float16",
+        bnb_4bit_use_double_quant=True,
+    )
+    model = AutoModelForCausalLM.from_pretrained(
+        "TheBloke/wizardLM-7B-HF",
+        trust_remote_code=True, 
+        device_map="auto", 
+        quantization_config=bnb_config,
+        cache_dir="./models",
+    )
+else:
+    model = AutoModelForCausalLM.from_pretrained(
+        "TheBloke/wizardLM-7B-HF",
+        # "WizardLM/WizardLM-13B-V1.2",
+        trust_remote_code=True, 
+        device_map="auto", 
+        load_in_8bit=True,
+        cache_dir="./models",
+    )
+
+
+
+# Load in the tokenizer
+tokenizer = AutoTokenizer.from_pretrained(
+    "TheBloke/wizardLM-7B-HF",
+    trust_remote_code=True,
+    cache_dir="./models",
+)
+tokenizer.pad_token = tokenizer.eos_token
+
+
+
+# Load in the dataset and map using the tokenizer
+if dataset_type == "reddit":
+    dataset = load_dataset(
+        "gmongaras/reddit_political_2019", 
+        cache_dir="./datasets",
+    )["train"]
+elif dataset_type == "reddit_negative":
+    dataset = load_dataset(
+        "gmongaras/reddit_negative",
+        cache_dir="./datasets",
+    )["train"]
+    
+    # Tokenize the dataset
+    def map_function(example):
+        # Encode the text
+        encoded = tokenizer(example["text"], max_length=max_length, truncation=True, padding="max_length")
+        
+        # Add on a pad token to the end of the input_ids
+        encoded["input_ids"] = encoded["input_ids"] + [tokenizer.pad_token_id]
+        encoded["attention_mask"] = encoded["attention_mask"] + [0]
+        
+        # Combine the attention masks. Attention masks are 0
+        # where we want to mask and 1 where we want to attend.
+        # We want to attend to both context and generated output
+        # Also add a 1 for a single padding
+        attention_mask = [1]*(sum(encoded["attention_mask"])+1) + [0]*(len(encoded["attention_mask"])-sum(encoded["attention_mask"])-1)
+        
+        # The labels are the input ids, but we want to mask the loss for the context and padding
+        labels = [-100 if encoded["attention_mask"][i] == 0 else encoded["input_ids"][i] for i in range(len(encoded["attention_mask"]))]
+        
+        return {
+            "input_ids": encoded["input_ids"],
+            "labels": labels,
+            "attention_mask": attention_mask
+        }
+    dataset = dataset.map(map_function)
+elif dataset_type == "squad":
+    # Load in the dataset and map using the tokenizer
+    dataset = load_dataset("squad")
+    """
+    The dataset has context, questions, and answers.
+
+    For this example, I am just encoding the question and first answer.
+    when you would actually want the context and question.
+
+    We want the text string to be in the format
+    #### Human: {question}#### Assistant: {output}
+
+    We want to turn this into the format:
+    {
+        "input_ids": input ids for the encoded instruction and input
+        "labels": This is the input ids, but we put -100 where we want to mask the
+                    loss. We want to mask the loss for the instruction, input, and padding.
+                    We use -100 because PyTorch CrossEntropy ignores -100 labels.
+        "attention_mask": attention mask so the model doesn't attend to padding
+    }
+    """
+    def map_function(example):
+        # Get the question and model output
+        question = f"#### Human: {example['question'].strip()}"
+        output = f"#### Assistant: {example['answers']['text'][0].strip()}"
+
+        # Encode the question and output
+        question_encoded = tokenizer(question)
+        output_encoded = tokenizer(output, max_length=max_length-1-len(question_encoded["input_ids"]), truncation=True, padding="max_length")
+
+        # Add on a pad token to the end of the input_ids
+        output_encoded["input_ids"] = output_encoded["input_ids"] + [tokenizer.pad_token_id]
+        output_encoded["attention_mask"] = output_encoded["attention_mask"] + [0]
+
+        # Combine the input ids
+        input_ids = question_encoded["input_ids"] + output_encoded["input_ids"]
+
+        # The labels are the input ids, but we want to mask the loss for the context and padding
+        labels = [-100]*len(question_encoded["input_ids"]) + [output_encoded["input_ids"][i] if output_encoded["attention_mask"][i] == 1 else -100 for i in range(len(output_encoded["attention_mask"]))]
+
+        # Combine the attention masks. Attention masks are 0
+        # where we want to mask and 1 where we want to attend.
+        # We want to attend to both context and generated output
+        # Also add a 1 for a single padding
+        attention_mask = [1]*len(question_encoded["input_ids"]) + [1]*(sum(output_encoded["attention_mask"])+1) + [0]*(len(output_encoded["attention_mask"])-sum(output_encoded["attention_mask"])-1)
+
+        return {
+            "input_ids": input_ids,
+            "labels": labels,
+            "attention_mask": attention_mask
+        }
+    dataset = dataset["train"].map(map_function)
+
+# Randomize data
+dataset = dataset.shuffle()
+
+# Test/train split
+train_size = int(len(dataset) * 0.8)
+test_size = len(dataset) - train_size
+data_train = dataset.select(range(train_size))
+data_test = dataset.select(range(train_size, train_size + test_size))
+
+
+# Adapt the model with LoRA weights
+peft_config = LoraConfig(
+    lora_alpha=lora_alpha,
+    lora_dropout=lora_dropout,
+    r=lora_r,
+    bias=lora_bias,
+    task_type="CAUSAL_LM",
+    inference_mode=False,
+    target_modules=lora_target_modules
+)
+model = get_peft_model(model, peft_config)
+model.print_trainable_parameters()
+
+
+training_args = TrainingArguments(
+    output_dir=output_dir,
+    evaluation_strategy="epoch",
+    optim=optim_type,
+    learning_rate=learning_rate,
+    weight_decay=weight_decay,
+    per_device_train_batch_size=per_device_train_batch_size,
+    per_device_eval_batch_size=per_device_eval_batch_size,
+    gradient_accumulation_steps=gradient_accumulation_steps,
+    do_train=True,
+    warmup_steps=warmup_steps,
+    save_steps=save_steps,
+    logging_steps=logging_steps,
+)
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=data_train,
+    eval_dataset=data_test,
+    tokenizer=tokenizer,
+)
+
+# Train the model
+trainer.train()
---
+++ merge.py
@@ -0,0 +1,57 @@
+import peft
+import torch
+from peft import PeftConfig, PeftModel
+from transformers import AutoModelForCausalLM, AutoTokenizer, HfArgumentParser
+import shutil
+
+
+
+lora_path = "outputs_reddit/checkpoint-700" # Path to the LoRA weights
+output_path = "outputs_reddit/merged_model" # Path to output the merged weights
+
+
+
+
+peft_model_id = lora_path
+peft_config = PeftConfig.from_pretrained(peft_model_id)
+model = AutoModelForCausalLM.from_pretrained(
+    peft_config.base_model_name_or_path,
+    return_dict=True,
+    torch_dtype=torch.float16,
+    trust_remote_code=True,
+    device_map="cpu",
+    cache_dir="./models"
+)
+tokenizer = AutoTokenizer.from_pretrained(peft_config.base_model_name_or_path, cache_dir="./models")
+
+# Copy local model to output path
+import os
+path = os.listdir("models/models--TheBloke--wizardLM-7B-HF/snapshots")[0]
+path = os.path.join("models/models--TheBloke--wizardLM-7B-HF/snapshots", path)
+shutil.copytree(path, output_path, dirs_exist_ok=True, ignore=shutil.ignore_patterns('*.pt', "*.pth", "*.bin"))
+
+# Load the Lora model
+model = PeftModel.from_pretrained(model, peft_model_id)
+model.eval()
+
+key_list = [key for key, _ in model.named_modules() if "lora" not in key]
+for key in key_list:
+    try:
+        sub_mod = model.get_submodule(key)
+        parent = model.get_submodule(".".join(key.split(".")[:-1]))
+    except AttributeError:
+        continue
+    target_name = key.split(".")[-1]
+    if isinstance(sub_mod, peft.tuners.lora.Linear):
+        sub_mod.merge()
+        bias = sub_mod.bias is not None
+        new_module = torch.nn.Linear(sub_mod.in_features, sub_mod.out_features, bias=bias)
+        new_module.weight.data = sub_mod.weight
+        if bias:
+            new_module.bias.data = sub_mod.bias
+        model.base_model._replace_module(parent, target_name, new_module, sub_mod)
+
+model = model.base_model.model
+
+# Save the model
+model.save_pretrained(output_path)
